8f18f0ffe4def4082165428b7c09c84ece1f4e4c
Chetan Shenoy <cshenoy@gmail.com>
2019-06-06 11:39:37 -0400

    Update app_funcs.sh

--- lib/app_funcs.sh
+++ lib/app_funcs.sh
@@ -18,6 +18,11 @@ function copy_hex() {
 
   # hex is a directory from elixir-1.3.0
   full_hex_file_path=$(ls -dt ${HOME}/.mix/archives/hex-* | head -n 1)
+  echo "^^^^^^^^^^^^^^^^"
+  echo ${HOME}
+  echo "!!!!!!!!!!!!!!!"
+  echo $full_hex_file_path
+  echo "@@@@@@@@@@@@@@@@"
   output_section "***** $full_hex_file_path"
   # hex file names after elixir-1.1 in the hex-<version>.ez form
   if [ -z "$full_hex_file_path" ]; then
